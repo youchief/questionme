d7f4647b3311e9ddb90b9793a6ddbe6ce08f42df
jeanvier <damien@3xw.ch>
2014-12-16 17:09:11 +0100

    Give to 3xw

--- app/Controller/GiftsController.php
+++ app/Controller/GiftsController.php
@@ -12,228 +12,234 @@ App::uses('CakeEmail', 'Network/Email');
  */
 class GiftsController extends AppController {
 
-        /**
-         * Components
-         *
-         * @var array
-         */
-        public $components = array('Paginator', 'Session');
-        public $uses = array('Gift', 'UsersChoice', 'User');
+    /**
+     * Components
+     *
+     * @var array
+     */
+    public $components = array('Paginator', 'Session');
+    public $uses = array('Gift', 'UsersChoice', 'User');
 
-        public function use_it($id = null) {
-                if (!$this->Gift->exists($id)) {
-                        throw new NotFoundException(__('Invalid BigGift'));
-                }
-
-                if ($this->request->is('post')) {
-                        $big = $this->Gift->findById($id);
-
-                        if ($big['Gift']['used'] <> null) {
-                                $this->Session->setFlash(__('Code déjà utilisé!'), 'default', array('class' => 'alert alert-danger'));
-                                $this->redirect(array('controller' => 'vouchers', 'action' => 'my_vouchers'));
-                        }
-
-                        if ($this->request->data['Gift']['code'] == $big['Customer']['code']) {
-                                $this->Gift->id = $id;
-                                $this->Gift->saveField('used', date('Y-m-d H:i:s'));
-                                $this->Session->setFlash(__('Merci d\'offir le cadeau !'), 'message_success');
-                                $this->redirect(array('action' => 'partner', $id));
-                        } else {
-                                $this->Session->setFlash(__('Code erroné ! Essaie encore !'), 'default', array('class' => 'alert alert-danger'));
-                                $this->redirect($this->referer());
-                        }
-                }
-
-
-                $options = array('conditions' => array('Gift.' . $this->Gift->primaryKey => $id));
-                $this->set('gift', $this->Gift->find('first', $options));
+    public function use_it($id = null) {
+        if (!$this->Gift->exists($id)) {
+            throw new NotFoundException(__('Invalid BigGift'));
         }
 
-        public function partner($gift_id) {
-                $gift = $this->Gift->findById($gift_id);
-                $this->set('voucher', $gift['Gift']['name']);
-                $this->set('customer', $gift['Customer']['name']);
-                $this->set('img', $gift['Gift']['media']);
-                $this->set('used', $gift['Gift']['used']);
+        if ($this->request->is('post')) {
+            $big = $this->Gift->findById($id);
 
-                $this->render('/vouchers/partner');
-        }
-
-        public function view($id = null) {
-                if (!$this->Gift->exists($id)) {
-                        throw new NotFoundException(__('Invalid gift'));
-                }
-                $options = array('conditions' => array('Gift.' . $this->Gift->primaryKey => $id));
-                $gift = $this->Gift->find('first', $options);
-                $this->set('gift', $gift);
-        }
-
-        public function delete($gift_id) {
-                $this->Gift->id = $gift_id;
-                $this->Gift->saveField('winner_id', null);
+            if ($big['Gift']['used'] <> null) {
+                $this->Session->setFlash(__('Code déjà utilisé!'), 'default', array('class' => 'alert alert-danger'));
                 $this->redirect(array('controller' => 'vouchers', 'action' => 'my_vouchers'));
-                $this->Session->setFlash(__('Cadeau effacé !'), 'message_danger');
-        }
+            }
 
-        /**
-         * admin_index method
-         *
-         * @return void
-         */
-        public function admin_index() {
-                $this->Gift->recursive = 1;
-                $this->set('gifts', $this->Paginator->paginate());
-        }
-
-        /**
-         * admin_view method
-         *
-         * @throws NotFoundException
-         * @param string $id
-         * @return void
-         */
-        public function admin_view($id = null) {
-                if (!$this->Gift->exists($id)) {
-                        throw new NotFoundException(__('Invalid gift'));
-                }
-                $options = array('conditions' => array('Gift.' . $this->Gift->primaryKey => $id));
-                $gift = $this->Gift->find('first', $options);
-                $winner = array('User' => array('username' => 'no body'));
-                if (!empty($gift['Gift']['winner_id'])) {
-                        $winner = $this->User->findById($gift['Gift']['winner_id']);
-                }
-                $this->set('winner', $winner);
-                $this->set('gift', $gift);
-        }
-
-        /**
-         * admin_add method
-         *
-         * @return void
-         */
-        public function admin_add() {
-                if ($this->request->is('post')) {
-                        $this->Gift->create();
-                        if ($this->Gift->save($this->request->data)) {
-                                $this->Session->setFlash(__('The gift has been saved'), 'default', array('class' => 'alert alert-success'));
-                                return $this->redirect(array('action' => 'index'));
-                        } else {
-                                $this->Session->setFlash(__('The gift could not be saved. Please, try again.'), 'default', array('class' => 'alert alert-error'));
-                        }
-                }
-                $customers = $this->Gift->Customer->find('list');
-                $qdays = $this->Gift->Qday->find('list');
-                $this->set(compact('customers', 'qdays'));
-        }
-
-        public function admin_duplicate($id) {
-                $gift = $this->Gift->findById($id);
-                $new_gift = array();
-                $new_gift['Gift']['name'] = $gift['Gift']['name'];
-                $new_gift['Gift']['description'] = $gift['Gift']['description'];
-                $new_gift['Gift']['validity'] = $gift['Gift']['validity'];
-                $new_gift['Gift']['conditions'] = $gift['Gift']['conditions'];
-                $new_gift['Gift']['media'] = $gift['Gift']['media'];
-                $new_gift['Gift']['customer_id'] = $gift['Gift']['customer_id'];
-                $new_gift['Gift']['qday_id'] = $gift['Gift']['qday_id'];
-                
-                $this->Gift->Behaviors->unload('Uploader.Attachment');
-                $this->Gift->Behaviors->unload('Uploader.FileValidation');
-                $this->Gift->create();
-                if ($this->Gift->save($new_gift)) {
-                        $this->Session->setFlash(__('The gift has been saved'), 'default', array('class' => 'alert alert-success'));
-                        return $this->redirect(array('action' => 'index'));
-                } else {
-                        $this->Session->setFlash(__('The gift could not be saved. Please, try again.'), 'default', array('class' => 'alert alert-error'));
-                }
-        }
-
-        /**
-         * admin_edit method
-         *
-         * @throws NotFoundException
-         * @param string $id
-         * @return void
-         */
-        public function admin_edit($id = null) {
-                if (!$this->Gift->exists($id)) {
-                        throw new NotFoundException(__('Invalid gift'));
-                }
-                if ($this->request->is('post') || $this->request->is('put')) {
-                        if ($this->Gift->save($this->request->data)) {
-                                $this->Session->setFlash(__('The gift has been saved'), 'default', array('class' => 'alert alert-success'));
-                                return $this->redirect(array('action' => 'index'));
-                        } else {
-                                $this->Session->setFlash(__('The gift could not be saved. Please, try again.'), 'default', array('class' => 'alert alert-error'));
-                        }
-                } else {
-                        $options = array('conditions' => array('Gift.' . $this->Gift->primaryKey => $id));
-                        $this->request->data = $this->Gift->find('first', $options);
-                }
-                $customers = $this->Gift->Customer->find('list');
-                $qdays = $this->Gift->Qday->find('list');
-                $this->set(compact('customers', 'qdays'));
-        }
-
-        /**
-         * admin_delete method
-         *
-         * @throws NotFoundException
-         * @param string $id
-         * @return void
-         */
-        public function admin_delete($id = null) {
+            if ($this->request->data['Gift']['code'] == $big['Customer']['code']) {
                 $this->Gift->id = $id;
-                if (!$this->Gift->exists()) {
-                        throw new NotFoundException(__('Invalid gift'));
-                }
-                $this->request->onlyAllow('post', 'delete');
-                if ($this->Gift->delete()) {
-                        $this->Session->setFlash(__('Gift deleted'), 'default', array('class' => 'alert alert-success'));
-                        return $this->redirect(array('action' => 'index'));
-                }
-                $this->Session->setFlash(__('Gift was not deleted'), 'default', array('class' => 'alert alert-error'));
+                $this->Gift->saveField('used', date('Y-m-d H:i:s'));
+                $this->Session->setFlash(__('Merci d\'offir le cadeau !'), 'message_success');
+                $this->redirect(array('action' => 'partner', $id));
+            } else {
+                $this->Session->setFlash(__('Code erroné ! Essaie encore !'), 'default', array('class' => 'alert alert-danger'));
+                $this->redirect($this->referer());
+            }
+        }
+
+
+        $options = array('conditions' => array('Gift.' . $this->Gift->primaryKey => $id));
+        $this->set('gift', $this->Gift->find('first', $options));
+    }
+
+    public function partner($gift_id) {
+        $gift = $this->Gift->findById($gift_id);
+        $this->set('voucher', $gift['Gift']['name']);
+        $this->set('customer', $gift['Customer']['name']);
+        $this->set('img', $gift['Gift']['media']);
+        $this->set('used', $gift['Gift']['used']);
+
+        $this->render('/vouchers/partner');
+    }
+
+    public function view($id = null) {
+        if (!$this->Gift->exists($id)) {
+            throw new NotFoundException(__('Invalid gift'));
+        }
+        $options = array('conditions' => array('Gift.' . $this->Gift->primaryKey => $id));
+        $gift = $this->Gift->find('first', $options);
+        $this->set('gift', $gift);
+    }
+
+    public function delete($gift_id) {
+        $this->Gift->id = $gift_id;
+        $this->Gift->saveField('winner_id', null);
+        $this->redirect(array('controller' => 'vouchers', 'action' => 'my_vouchers'));
+        $this->Session->setFlash(__('Cadeau effacé !'), 'message_danger');
+    }
+
+    /**
+     * admin_index method
+     *
+     * @return void
+     */
+    public function admin_index() {
+        $this->Gift->recursive = 1;
+        $this->set('gifts', $this->Paginator->paginate());
+    }
+
+    /**
+     * admin_view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function admin_view($id = null) {
+        if (!$this->Gift->exists($id)) {
+            throw new NotFoundException(__('Invalid gift'));
+        }
+        $options = array('conditions' => array('Gift.' . $this->Gift->primaryKey => $id));
+        $gift = $this->Gift->find('first', $options);
+        $winner = array('User' => array('username' => 'no body'));
+        if (!empty($gift['Gift']['winner_id'])) {
+            $winner = $this->User->findById($gift['Gift']['winner_id']);
+        }
+        $this->set('winner', $winner);
+        $this->set('gift', $gift);
+    }
+
+    /**
+     * admin_add method
+     *
+     * @return void
+     */
+    public function admin_add() {
+        if ($this->request->is('post')) {
+            $this->Gift->create();
+            if ($this->Gift->save($this->request->data)) {
+                $this->Session->setFlash(__('The gift has been saved'), 'default', array('class' => 'alert alert-success'));
                 return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The gift could not be saved. Please, try again.'), 'default', array('class' => 'alert alert-error'));
+            }
         }
+        $customers = $this->Gift->Customer->find('list');
+        $qdays = $this->Gift->Qday->find('list');
+        $this->set(compact('customers', 'qdays'));
+    }
 
-        public function admin_draw($id) {
-                $gift = $this->Gift->findById($id);
+    public function admin_duplicate($id) {
+        $gift = $this->Gift->findById($id);
+        $new_gift = array();
+        $new_gift['Gift']['name'] = $gift['Gift']['name'];
+        $new_gift['Gift']['description'] = $gift['Gift']['description'];
+        $new_gift['Gift']['validity'] = $gift['Gift']['validity'];
+        $new_gift['Gift']['conditions'] = $gift['Gift']['conditions'];
+        $new_gift['Gift']['media'] = $gift['Gift']['media'];
+        $new_gift['Gift']['customer_id'] = $gift['Gift']['customer_id'];
+        $new_gift['Gift']['qday_id'] = $gift['Gift']['qday_id'];
 
-                $user_choices = $this->UsersChoice->find('all', array(
-                    'conditions' => array(
-                        'UsersChoice.created >' => $gift['Qday']['start'],
-                        'UsersChoice.created <' => $gift['Qday']['end'],
-                    ),
-                    'fields' => array('DISTINCT UsersChoice.question_id', 'UsersChoice.user_id'),
-                        )
-                );
-
-                shuffle($user_choices);
-
-                $winner = $this->UsersChoice->User->findById($user_choices[0]['UsersChoice']['user_id']);
-
-                $this->set('user', $winner);
-                $this->set('gift_id', $id);
+        $this->Gift->Behaviors->unload('Uploader.Attachment');
+        $this->Gift->Behaviors->unload('Uploader.FileValidation');
+        $this->Gift->create();
+        if ($this->Gift->save($new_gift)) {
+            $this->Session->setFlash(__('The gift has been saved'), 'default', array('class' => 'alert alert-success'));
+            return $this->redirect(array('action' => 'index'));
+        } else {
+            $this->Session->setFlash(__('The gift could not be saved. Please, try again.'), 'default', array('class' => 'alert alert-error'));
         }
+    }
 
-        public function admin_validate($user_id, $gift_id) {
-                $gift = $this->Gift->findById($gift_id);
-                $user = $this->User->findById($user_id);
-
-                $this->Gift->id = $gift_id;
-                $this->Gift->saveField('winner_id', $user_id);
-                $this->Session->setFlash(__('The gift has been given'), 'default', array('class' => 'alert alert-success'));
-
-                $Email = new CakeEmail('smtp');
-                $Email->from(array('hello@questoionme.ch' => 'Question Me'));
-                $Email->to($user['User']['email']);
-                $Email->subject('T\'es un winner !');
-                $Email->viewVars(array('user' => $user['User']['username'], 'gift' => $gift['Gift']['name'], 'link' => 'http://www.questionme.ch/vouchers/my_vouchers'));
-                $Email->emailFormat('html');
-                $Email->template('winner');
-                $Email->send();
-
-                $this->redirect(array('action' => 'index'));
+    /**
+     * admin_edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function admin_edit($id = null) {
+        if (!$this->Gift->exists($id)) {
+            throw new NotFoundException(__('Invalid gift'));
         }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->Gift->save($this->request->data)) {
+                $this->Session->setFlash(__('The gift has been saved'), 'default', array('class' => 'alert alert-success'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The gift could not be saved. Please, try again.'), 'default', array('class' => 'alert alert-error'));
+            }
+        } else {
+            $options = array('conditions' => array('Gift.' . $this->Gift->primaryKey => $id));
+            $this->request->data = $this->Gift->find('first', $options);
+        }
+        $customers = $this->Gift->Customer->find('list');
+        $qdays = $this->Gift->Qday->find('list');
+        $this->set(compact('customers', 'qdays'));
+    }
+
+    /**
+     * admin_delete method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function admin_delete($id = null) {
+        $this->Gift->id = $id;
+        if (!$this->Gift->exists()) {
+            throw new NotFoundException(__('Invalid gift'));
+        }
+        $this->request->onlyAllow('post', 'delete');
+        if ($this->Gift->delete()) {
+            $this->Session->setFlash(__('Gift deleted'), 'default', array('class' => 'alert alert-success'));
+            return $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Gift was not deleted'), 'default', array('class' => 'alert alert-error'));
+        return $this->redirect(array('action' => 'index'));
+    }
+
+    public function admin_draw($id) {
+        $gift = $this->Gift->findById($id);
+
+        $user_choices = $this->UsersChoice->find('all', array(
+            'conditions' => array(
+                'UsersChoice.created >' => $gift['Qday']['start'],
+                'UsersChoice.created <' => $gift['Qday']['end'],
+            ),
+            'fields' => array('DISTINCT UsersChoice.question_id', 'UsersChoice.user_id'),
+                )
+        );
+
+        shuffle($user_choices);
+
+        $winner = $this->UsersChoice->User->findById($user_choices[0]['UsersChoice']['user_id']);
+
+        $this->set('user', $winner);
+        $this->set('gift_id', $id);
+    }
+
+    public function admin_validate($user_id, $gift_id) {
+        $gift = $this->Gift->findById($gift_id);
+        $user = $this->User->findById($user_id);
+
+        $this->Gift->id = $gift_id;
+        $this->Gift->saveField('winner_id', $user_id);
+        $this->Session->setFlash(__('The gift has been given'), 'default', array('class' => 'alert alert-success'));
+
+        $Email = new CakeEmail('smtp');
+        $Email->from(array('hello@questoionme.ch' => 'Question Me'));
+        $Email->to($user['User']['email']);
+        $Email->subject('T\'es un winner !');
+        $Email->viewVars(array('user' => $user['User']['username'], 'gift' => $gift['Gift']['name'], 'link' => 'http://www.questionme.ch/vouchers/my_vouchers'));
+        $Email->emailFormat('html');
+        $Email->template('winner');
+        $Email->send();
+
+        $this->redirect(array('action' => 'index'));
+    }
+
+    public function admin_give_to_3xw($gift_id, $user_id) {
+        $this->Gift->id = $gift_id;
+        $this->Gift->saveField('winner_id', $user_id);
+        $this->Session->setFlash(__('Oui... vous avez fait le bon choix !'), 'default', array('class' => 'alert alert-success'));
+    }
 
 }
--- app/View/Gifts/admin_view.ctp
+++ app/View/Gifts/admin_view.ctp
@@ -1,70 +1,75 @@
 <div class="gifts view">
-        <div class="row">
-                <div class='col-sm-6'>
-                        <div class='thumbnail gift'>
-                                <i class="fa fa-gift"> Cadeau</i>  
-                                <?php echo $this->Html->image($gift['Gift']['media'], array('class' => 'img-responsive')) ?>
-                                <h3><?php echo $gift['Gift']['name'] ?></h3>
-                                <p>
-                                        <?php
-                                        echo $this->Text->truncate($gift['Gift']['description'], 150, array('ellipsis' => '...', 'exact' => false));
-                                        ?>
-                                </p>
+    <div class="row">
+        <div class='col-sm-6'>
+            <div class='thumbnail gift'>
+                <i class="fa fa-gift"> Cadeau</i>  
+                <?php echo $this->Html->image($gift['Gift']['media'], array('class' => 'img-responsive')) ?>
+                <h3><?php echo $gift['Gift']['name'] ?></h3>
+                <p>
+                    <?php
+                    echo $this->Text->truncate($gift['Gift']['description'], 150, array('ellipsis' => '...', 'exact' => false));
+                    ?>
+                </p>
 
-                        </div>
-                </div>
-                <div class="col-sm-6">
-                        <dl>
-                                <dt><?php echo __('Id'); ?></dt>
-                                <dd>
-                                        <?php echo h($gift['Gift']['id']); ?>
-                                        &nbsp;
-                                </dd>
-                                <dt><?php echo __('Created'); ?></dt>
-                                <dd>
-                                        <?php echo h($gift['Gift']['created']); ?>
-                                        &nbsp;
-                                </dd>
-
-
-                                <dt><?php echo __('Validity'); ?></dt>
-                                <dd>
-                                        <?php echo h($gift['Gift']['validity']); ?>
-                                        &nbsp;
-                                </dd>
-                                <dt><?php echo __('Conditions'); ?></dt>
-                                <dd>
-                                        <?php echo $gift['Gift']['conditions']; ?>
-                                        &nbsp;
-                                </dd>
-                                <dt><?php echo __('Used'); ?></dt>
-                                <dd>
-                                        <?php echo h($gift['Gift']['used']); ?>
-                                        &nbsp;
-                                </dd>
-                                <dt><?php echo __('Customer'); ?></dt>
-                                <dd>
-                                        <?php echo $this->Html->link($gift['Customer']['name'], array('controller' => 'customers', 'action' => 'view', $gift['Customer']['id'])); ?>
-                                        &nbsp;
-                                </dd>
-                                <dt><?php echo __('Winner Id'); ?></dt>
-                                <dd>
-                                        <?php echo $this->Html->link($winner['User']['username'], array('controller' => 'users', 'action' => 'view', $winner['User']['id'])); ?>
-                                        &nbsp;
-                                </dd>
-                                <dt><?php echo __('Qday'); ?></dt>
-                                <dd>
-                                        <?php echo $this->Html->link($gift['Qday']['start'], array('controller' => 'qdays', 'action' => 'view', $gift['Qday']['id'])); ?>
-                                        &nbsp;
-                                </dd>
-                        </dl>
-                        <div class="btn-group">
-                                <?php echo $this->Html->link(__('Edit Gift'), array('action' => 'edit', $gift['Gift']['id']), array('class' => 'btn btn-sm btn-default')); ?>
-                                <?php echo $this->Form->postLink(__('Delete Gift'), array('action' => 'delete', $gift['Gift']['id']), array('class' => 'btn btn-sm btn-danger'), __('Are you sure you want to delete # %s?', $gift['Gift']['id'])); ?> 
-                                <?php echo $this->Html->link(__('Drawing lots'), array('action' => 'draw', $gift['Gift']['id']), array('class' => 'btn btn-sm btn-success')); ?>
-
-                        </div>
-                </div>
+            </div>
         </div>
+        <div class="col-sm-6">
+            <dl>
+                <dt><?php echo __('Id'); ?></dt>
+                <dd>
+                    <?php echo h($gift['Gift']['id']); ?>
+                    &nbsp;
+                </dd>
+                <dt><?php echo __('Created'); ?></dt>
+                <dd>
+                    <?php echo h($gift['Gift']['created']); ?>
+                    &nbsp;
+                </dd>
+
+
+                <dt><?php echo __('Validity'); ?></dt>
+                <dd>
+                    <?php echo h($gift['Gift']['validity']); ?>
+                    &nbsp;
+                </dd>
+                <dt><?php echo __('Conditions'); ?></dt>
+                <dd>
+                    <?php echo $gift['Gift']['conditions']; ?>
+                    &nbsp;
+                </dd>
+                <dt><?php echo __('Used'); ?></dt>
+                <dd>
+                    <?php echo h($gift['Gift']['used']); ?>
+                    &nbsp;
+                </dd>
+                <dt><?php echo __('Customer'); ?></dt>
+                <dd>
+                    <?php echo $this->Html->link($gift['Customer']['name'], array('controller' => 'customers', 'action' => 'view', $gift['Customer']['id'])); ?>
+                    &nbsp;
+                </dd>
+                <dt><?php echo __('Winner Id'); ?></dt>
+                <dd>
+                    <?php echo $this->Html->link($winner['User']['username'], array('controller' => 'users', 'action' => 'view', $winner['User']['id'])); ?>
+                    &nbsp;
+                </dd>
+                <dt><?php echo __('Qday'); ?></dt>
+                <dd>
+                    <?php echo $this->Html->link($gift['Qday']['start'], array('controller' => 'qdays', 'action' => 'view', $gift['Qday']['id'])); ?>
+                    &nbsp;
+                </dd>
+            </dl>
+            <div class="btn-group">
+                <?php echo $this->Html->link(__('Edit Gift'), array('action' => 'edit', $gift['Gift']['id']), array('class' => 'btn btn-sm btn-default')); ?>
+                <?php echo $this->Form->postLink(__('Delete Gift'), array('action' => 'delete', $gift['Gift']['id']), array('class' => 'btn btn-sm btn-danger'), __('Are you sure you want to delete # %s?', $gift['Gift']['id'])); ?> 
+                <?php echo $this->Html->link(__('Drawing lots'), array('action' => 'draw', $gift['Gift']['id']), array('class' => 'btn btn-sm btn-success')); ?>
+
+            </div>
+            <div class="spacer--alpha"></div>
+            <div class="btn-group">
+                <?php echo $this->Html->link(__('Donner à CYCY'), array('action' => 'give_to_3xw', $gift['Gift']['id'], 41), array('class' => 'btn btn-lg btn-primary')); ?>
+                <?php echo $this->Html->link(__('Donner à DADA'), array('action' => 'give_to_3xw', $gift['Gift']['id'], 39), array('class' => 'btn btn-lg btn-primary')); ?>
+            </div>
+        </div>
+    </div>
 
 </div>
